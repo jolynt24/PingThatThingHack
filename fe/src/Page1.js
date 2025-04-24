1fa9838cb7a3a7e230ac4008f89a10ed5826354b
Robert Johnson <sc22r2j@leeds.ac.uk>
2025-04-24 13:53:48 +0100

    The Buttons in Page1 now lead to dashboard

--- fe/src/Page1.js
+++ fe/src/Page1.js
@@ -5,28 +5,36 @@ import './Page1.css'; // Assuming you'll add styles here
 function Page1() {
   const navigate = useNavigate();
 
-  const handleNavigation = (path) => {
-    navigate(path); // Navigate to the specified path
+  const handleNavigation = () => {
+    navigate('/Dashboard'); // Navigate to the "dashboard" page
   };
 
   return (
-  <div>
-      <header class="dashboard-header">
+    <div>
+      <header className="dashboard-header">
         <h1>Welcome to Cinder</h1>
         <p>Click anywhere to get started</p>
       </header>
-      <div class="dashboard-grid">
-        <div class="grid-item">
-          <button class="grid-button">I want to Travel</button>
+      <div className="dashboard-grid">
+        <div className="grid-item">
+          <button className="grid-button" onClick={handleNavigation}>
+            I want to Travel
+          </button>
         </div>
-        <div class="grid-item">
-          <button class="grid-button">I want to Start an ISA</button>
+        <div className="grid-item">
+          <button className="grid-button" onClick={handleNavigation}>
+            I want to Start an ISA
+          </button>
         </div>
-        <div class="grid-item">
-          <button class="grid-button">I want to get Phone Insurance</button>
+        <div className="grid-item">
+          <button className="grid-button" onClick={handleNavigation}>
+            I want to get Phone Insurance
+          </button>
         </div>
-        <div class="grid-item">
-          <button class="grid-button">I want to Get Club Lloyds</button>
+        <div className="grid-item">
+          <button className="grid-button" onClick={handleNavigation}>
+            I want to Get Club Lloyds
+          </button>
         </div>
       </div>
     </div>
