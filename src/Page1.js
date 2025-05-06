77d3118ba4b772ff926f238fc38912558bccdc05
Jolyn Tellis <jolyntellis@gmail.com>
2025-05-06 17:56:04 +0100

    chore: Refactor repo to host

---
+++ src/Page1.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Page1.css'; // Assuming you'll add styles here
+
+function Page1() {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate('/Dashboard'); // Navigate to the "dashboard" page
+  };
+
+  return (
+    <div>
+      <header className="dashboard-header">
+        <h1>Welcome to Cinder</h1>
+        <p className='subtitle'>Click anywhere to get started</p>
+      </header>
+      <div className="dashboard-grid">
+        <div className="grid-item">
+          <button className="grid-button" onClick={handleNavigation}>
+            I want to Travel
+          </button>
+        </div>
+        <div className="grid-item">
+          <button className="grid-button" onClick={handleNavigation}>
+            I want to Start an ISA
+          </button>
+        </div>
+        <div className="grid-item">
+          <button className="grid-button" onClick={handleNavigation}>
+            I want to get Phone Insurance
+          </button>
+        </div>
+        <div className="grid-item">
+          <button className="grid-button" onClick={handleNavigation}>
+            I want to Get Club Lloyds
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page1;
